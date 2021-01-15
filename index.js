70b96d97c96ebdfbe4cd56420bfeb068c93e46f2
Zach <zbayard@gmail.com>
2021-01-14 22:44:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+let formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  };
+  
+  // method: "POST" is missing from the object below
+  let configObj = {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    }); 
+
+
+
+    function submitData(name, email){
+
+        return fetch("http://localhost:3000/users" , {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name, email
+            })
+        })
+        .then(response => response.json())
+        .then(data => document.body.innerHTML = data["id"])
+        .catch(error => document.body.innerHTML = error.message)
+    }
